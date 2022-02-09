b4811047b768ea7383d3e46845055d66457e4146
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-02-09 12:12:37 +0000

    board

---
+++ src/Components/Board/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css"
+
+function Board(){
+    return (<div className="container">
+  <div className="Top-Left"></div>
+  <div className="Top-Middle"></div>
+  <div className="Top-Right"></div>
+  <div className="Middle-Left"></div>
+  <div className="Centre"></div>
+  <div className="Middle-right"></div>
+  <div className="Bottom-Left"></div>
+  <div className="Bottom-Middle"></div>
+  <div className="Bottom-Right"></div>
+    </div>)
+}
+export default Board
